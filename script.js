8bba3726724f70df8682f67bdbacb9d6cc463d55
Ian Hancock <northroadian@gmail.com>
2021-02-01 15:36:34 -0600

    adding contact form to page will make into a pop up later not finished

---
+++ script.js
@@ -0,0 +1,34 @@
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const businessName = document.getElementById('business');
+const phoneNumber = document.getElementById('phonenumber')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submit', (event)=>{
+    let messages =[] // this means empty array.
+    //below if no one passes in anything in the input box it will send a message
+    if (name.value === '' || name.value == null){
+        messages.push('Name is required') // this pushes this to the empty array of the variable messages
+    }
+    if (email.length <= 5){
+        messages.push 
+    }
+    if(messages.length > 0){
+        event.preventDefault()
+        errorElement.innerText = messages.join(',')
+    }
+    
+    
+})
+
+
+// notes
+// selecting get elementById selects the elements Id on the html side
+
+// the add event listener is listing for the event of submit 
+
+// the event.preventDefault preventDefault() method cancels the event if it is cancelable,
+// meaning that the default action that belongs to the event will not occur.
+
+
